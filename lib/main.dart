266e124f8696a6293d06787660f88a2ed89a7b9d
oski3 <osmancanboga@hotmail.com>
2021-04-21 12:08:02 +0300

    first project work

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hi First Project',
       theme: ThemeData(
+
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
